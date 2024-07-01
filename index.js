cb722f2e58be4f46dae8eabafda524e8aa80d088
Samuel Kinyua <samwakash@gmail.com>
2024-07-01 01:17:49 -0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Samuel Kinyua";
+//const welcomeMessage = "Welcome to Flatbook, ";
+//const welcomeMessage = currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = `Welcome, ${currentUser}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
